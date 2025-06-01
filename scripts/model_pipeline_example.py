9d1368997505ba1fb9565e30f533952615879090
Denys Denysyev <dvdenysyev@gmail.com>
2025-06-01 19:23:56 +0200

    First lab

---
+++ scripts/model_pipeline_example.py
@@ -0,0 +1,54 @@
+import logging
+
+from pipeline.download import download_and_extract
+from pipeline.ingestion import process_data
+from pipeline.loader import create_data_loader
+from pipeline.models import ResNetClassifier
+from pipeline.training import train_model
+from pipeline.testing import test_model
+
+import torch
+from torch import nn
+from torch import optim
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Configuration
+    config = {
+        'test_size': 0.2,
+        'val_size': 0.2,
+        'random_state': 42,
+        "lr": 0.001
+        # Add other configuration parameters as needed
+    }
+    # Step 1: Download data
+    cifar10_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    cifar10_dir = download_and_extract(cifar10_url, "./data/")
+
+    # Step 2: Process data
+    train_df, val_df, test_df = process_data(cifar10_dir, config)
+
+    # Step 3: Create data loaders
+    train_loader = create_data_loader(train_df, config)
+    val_loader = create_data_loader(val_df, config)
+    test_loader = create_data_loader(test_df, config)
+
+    # Step 4: Define model, loss function, optimizer
+    model = ResNetClassifier(n_classes=10).to(device)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=config["lr"])
+
+    # Step 5: Train model
+    best_model_path = train_model(
+        model, train_loader, val_loader, loss_function, optimizer, num_epochs=15, device=device)
+
+    # Step 6: Test model
+    test_model(model, test_loader, loss_function, device)
+
+
+if __name__ == "__main__":
+    main()
